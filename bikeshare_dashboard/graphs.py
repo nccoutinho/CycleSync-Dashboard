f433777487f3ed1d118e4e643ec3a55544071e2f
nccoutinho <natscc@student.ubc.ca>
2024-02-21 18:46:30 -0800

    Added data

--- bikeshare_dashboard/graphs.py
+++ bikeshare_dashboard/graphs.py
@@ -1,4 +1,33 @@
+import pandas as pd
+import os
+import dash
+import plotly.express as px
+import altair as alt
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
 
+# Path to the folder containing your files
+folder_path = './dataset'
+
+# Initialize an empty list to store DataFrames
+dfs = []
+
+# Iterate over each file in the folder
+for filename in os.listdir(folder_path):
+    if filename.endswith('.csv'):  # Assuming all files are CSV, you can change the condition accordingly
+        file_path = os.path.join(folder_path, filename)
+        # Try different encodings to read the file
+        for encoding in ['utf-8', 'latin-1']:  # You can add more encodings to try if needed
+            try:
+                df = pd.read_csv(file_path, encoding=encoding)
+                dfs.append(df)  # Append the DataFrame to the list
+                break  # Break the loop if reading is successful
+            except UnicodeDecodeError:
+                print(f"Error decoding file {filename} with encoding {encoding}. Trying another encoding...")
+
+# Concatenate all DataFrames in the list into one
+combined_df = pd.concat(dfs, ignore_index=True)
 
 # Remove null records
 combined_df = combined_df.dropna(subset=['Departure station'])
