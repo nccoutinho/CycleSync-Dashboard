f8f8aeecf203dc2ff52f5f99c8b0ef22281557d0
Christopher Mulya <cprasetya2000@gmail.com>
2024-02-27 18:00:49 -0800

    adjusted layout

--- bikeshare_dashboard/trends.py
+++ bikeshare_dashboard/trends.py
@@ -88,3 +88,84 @@ sort_table_1 = dcc.Dropdown(
    ],
    value='total'
 )
+
+# CHARTS / TABLE
+map_plot = dbc.Card(
+    [
+        # dbc.CardHeader(
+        #     html.H4("Geospatial Bike Concentration Plot", className="card-title", style={"color": "white"}),
+        #     style={"background-color": "#D80808"}
+        # ),
+        dbc.CardBody(
+            dbc.Col([
+                dcc.Loading(
+                    id="loading-1",
+                    type="circle",
+                    children=html.Iframe(
+                        id="polar_chart",
+                        style={
+                            "height": "22rem",
+                            "width": "100%",
+                            "border": "0",
+                            "display": "flex",
+                            "align-items": "center",
+                            "justify-content": "center"
+                            }
+                        ),
+                    color="#D80808"
+                )
+            ])
+        )
+    ],
+    className="mb-3",
+    style={
+        "width": "90%",
+        #"height": "90%",
+        "margin-left": "auto",
+        "border": "1px solid lightgray",
+        "box-shadow": "0px 1px 4px 0px rgba(0, 0, 0, 0.1)"
+    }
+)
+
+# LAYOUT
+app.layout = html.Div(
+    [
+        dcc.Location(id='url', refresh=False),  # Location component to track the URL
+        sidebar,
+        html.Div(
+            [
+                html.Hr(),
+                html.Div(
+                    [
+                        html.H6("Page / ", style={'display': 'inline'}),
+                        html.Span(id='current-page', style={'font-weight': 'bold'})
+                    ],
+                    className='top-bar',
+                    style={'margin-bottom': '20px'}  # Add vertical space between the sidebar and top bar
+                ),
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            [
+                                html.H5("View:"),
+                                sort_table_1,
+                                html.H5("Bike type:"),
+                                sort_table_2
+                            ],
+                            width=2,
+                            style={'margin-right': '20px'}  # Add horizontal space between top bar and sort tables
+                        ),
+                        dbc.Col(
+                            map_plot,
+                            width=9
+                        ),
+                    ],
+                    justify="center",
+                    style={'margin-top': '20px'}  # Add vertical space between top bar and sort tables/map_plot
+                ),
+                html.Hr()
+            ],
+            style={"margin": "0", "margin-left": "220px", "padding-left": "20px"}  # Adjusted styles for better alignment
+        ),
+    ]
+)
