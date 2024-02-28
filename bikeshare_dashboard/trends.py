5403aec6725dddaa30fb8d522e5969dee7b1e642
Christopher Mulya <cprasetya2000@gmail.com>
2024-02-27 17:59:13 -0800

    new py file for trends tab

---
+++ bikeshare_dashboard/trends.py
@@ -0,0 +1,46 @@
+from dash import dash, dash_table, callback, html, dcc, Input, Output
+import dash_bootstrap_components as dbc
+import dash_mantine_components as dmc
+import pandas as pd
+import altair as alt
+from datetime import date
+
+alt.data_transformers.disable_max_rows()
+
+# Read in data globally
+#data = pd.read_csv('../dataset/Mobi_System_Data_2023-01.csv', parse_dates=True, index_col=0)
+
+# Setup app and layout/frontend
+app = dash.Dash(
+    __name__,
+    external_stylesheets=[
+        "https://fonts.googleapis.com/css2?family=Assistant:wght@300&display=swap",
+        dbc.icons.FONT_AWESOME,
+        dbc.themes.JOURNAL,
+    ]
+)
+
+# Server
+server = app.server
+
+# Title
+app.title = 'Bikeshare Dashboard'
+
+# HEADER
+header = html.Div(
+    id="app-header",
+    children=[
+        html.H1(
+            "Bikeshare Dashboard",
+            style={
+                "display": "inline",
+                "font-size": "1.5em",
+                "margin-left": "1.8px",
+                "color": "white",  # Set font color to white
+                "background-color": "#D80808",  # Set background color to red
+                "padding": "10px"  # Add padding for better appearance
+            }
+        )
+    ],
+    style={"align": "center", "margin-left": 15}
+)
