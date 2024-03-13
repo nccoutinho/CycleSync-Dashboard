5f36987bd31f36dcade123db20ab76ea3302b996
Pranav Kumar Mahesh <pranava.km@gmail.com>
2024-03-13 12:18:37 -0700

    updated browser icon

--- bikeshare_dashboard/src/app.py
+++ bikeshare_dashboard/src/app.py
@@ -30,6 +30,25 @@ server = app.server
 
 # Title
 app.title = 'CycleSync'
+app.index_string = '''
+<!DOCTYPE html>
+<html>
+    <head>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>
+'''
 
 # --------------------------------------
 # HEADER
